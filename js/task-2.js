96d0a7b1660ebfc3e20dfea13fdce162c4793604
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-04-19 00:54:29 +0200

    new1

---
+++ js/task-2.js
@@ -0,0 +1,30 @@
+class Storage {
+  #items;
+  constructor(params) {
+    this.#items = params;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(item) {
+    this.#items.push(item);
+  }
+  removeItem(item) {
+    if (this.#items.includes(item)) {
+      return this.#items.splice(this.#items.indexOf(item), 1);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
